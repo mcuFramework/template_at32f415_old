8f3a48e94810e0b5bf153116484bbcd544026c2d
ZxyKira <danny41553@gmail.com>
2022-02-23 01:09:18 +0800

    [feat] template

---
+++ src/resource/core_arterytek_interrupt.cpp
@@ -0,0 +1,297 @@
+/**
+ * Copyright (c) 2020 ZxyKira
+ * All rights reserved.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
+/* ****************************************************************************************
+ * Include
+ */
+#include "core/arterytek/at32f415/Core.h" 
+#include "core/arterytek/at32f415/CoreInterrupt.h" 
+
+#include "bsp_arterytek_at32f415/at32f415.h"
+#include "bsp_arterytek_at32f415/core_cm4.h"
+#include "bsp_arterytek_at32f415/at32f415_exint.h"
+
+/* ****************************************************************************************
+ * Using
+ */  
+using core::arterytek::at32f415::Core;
+using core::arterytek::at32f415::CoreInterrupt;
+
+/* ****************************************************************************************
+ * Macro
+ */
+
+/* ****************************************************************************************
+ * Extern
+ */
+
+/* ****************************************************************************************
+ * Struct
+ */
+
+/* ****************************************************************************************
+ * Static Variable
+ */
+ 
+/* ****************************************************************************************
+ * Variable
+ */
+
+/* ****************************************************************************************
+ * List
+ */
+ 
+/**
+ * Inline function list
+ */
+ 
+/**
+ * Static function list
+ */
+
+/* ****************************************************************************************
+ * Inline Function
+ */
+
+/* ****************************************************************************************
+ * Static Function
+ */
+
+/* ****************************************************************************************
+ * Function
+ */
+ 
+/* ****************************************************************************************
+ * Function - SPI/I2S
+ */
+
+/**
+ *
+ */
+extern "C" void SPI1_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_SPI1]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void SPI2_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_SPI2]->interruptEvent();
+}
+
+/* ****************************************************************************************
+ * Function - USART
+ */
+
+/**
+ *
+ */
+extern "C" void USART1_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_USART1]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void USART2_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_USART2]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void USART3_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_USART3]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void UART4_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_UART4]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void UART5_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_UART5]->interruptEvent();
+}
+
+/* ****************************************************************************************
+ * Function - TMR
+ */
+
+/**
+ *
+ */
+extern "C" void TMR1_CH_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR1_CC]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void TMR1_BRK_TMR9_IRQHandler(void){
+  if(TMR1->ists_bit.brkif)
+    Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR1_BRK]->interruptEvent();
+  
+  if(TMR9->ists)
+    Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR9]->interruptEvent();
+
+}
+
+/**
+ *
+ */
+extern "C" void TMR1_OVF_TMR10_IRQHandler(void){
+  if(TMR1->ists_bit.ovfif)
+    Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR1]->interruptEvent();
+    
+  
+  if(TMR10->ists)
+     Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR10]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void TMR1_TRG_HALL_TMR11_IRQHandler(void){
+  if(TMR1->ists_bit.trgif)
+    Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR1_TRG_COM]->interruptEvent();
+  
+  if(TMR11->ists)
+    Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR11]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void TMR2_GLOBAL_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR2]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void TMR3_GLOBAL_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR3]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void TMR4_GLOBAL_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR4]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void TMR5_GLOBAL_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_TMR5]->interruptEvent();
+}
+
+
+
+/* ****************************************************************************************
+ * Function - EXINT
+ */
+
+/**
+ *
+ */
+extern "C" void EXINT0_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT0]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void EXINT1_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT1]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void EXINT2_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT2]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void EXINT3_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT3]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void EXINT4_IRQHandler(void){
+  Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT4]->interruptEvent();
+}
+
+/**
+ *
+ */
+extern "C" void EXINT9_5_IRQHandler(void){
+  for(;;){
+		if(EXINT->intsts & 0x00000020)
+      Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT5]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00000040)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT6]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00000080)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT7]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00000100)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT8]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00000200)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT9]->interruptEvent();
+		
+		else
+			break;
+	}
+}
+
+/**
+ *
+ */
+extern "C" void EXINT15_10_IRQHandler(void){
+	for(;;){
+		if(EXINT->intsts & 0x00000400)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT10]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00000800)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT11]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00001000)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT12]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00002000)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT13]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00004000)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT14]->interruptEvent();
+		
+		else if(EXINT->intsts & 0x00008000)
+			Core::interrupt.mHandle[CoreInterrupt::IRQ_EXINT15]->interruptEvent();
+		
+		else
+			break;
+	}  
+}
+
+
+
+/* *****************************************************************************************
+ *    End of file
+ */
---
+++ src/resource/core_arterytek_interrupt_priority.c
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2020 ZxyKira
+ * All rights reserved.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
+/* ****************************************************************************************
+ * Include
+ */  
+#include "bsp_arterytek_at32f415/at32f415.h"
+#include "bsp_arterytek_at32f415/core_cm4.h"
+
+/* ****************************************************************************************
+ * Macro
+ */
+ 
+/* ****************************************************************************************
+ * Type/Structure
+ */ 
+typedef struct _nvic_priority_level_t{
+  IRQn_Type irq;
+  uint8_t priority;
+}nvic_priority_level_t;
+
+/* ****************************************************************************************
+ * Extern
+ */
+
+/* ****************************************************************************************
+ * Public
+ */
+
+/* ****************************************************************************************
+ * Private
+ */
+const nvic_priority_level_t nvic_priority_level[] = {
+  {WWDT_IRQn,               15},
+  {PVM_IRQn,                15},
+  {TAMP_STAMP_IRQn,         15},
+  {ERTC_WKUP_IRQn,          15},
+  {FLASH_IRQn,              15},
+  {CRM_IRQn,                15},
+  {EXINT0_IRQn,             15},
+  {EXINT1_IRQn,             15},
+  {EXINT2_IRQn,             15},
+  {EXINT3_IRQn,             15},
+  {EXINT4_IRQn,             15},
+  {DMA1_Channel1_IRQn,      15},
+  {DMA1_Channel2_IRQn,      15},
+  {DMA1_Channel3_IRQn,      15},
+  {DMA1_Channel4_IRQn,      15},
+  {DMA1_Channel5_IRQn,      15},
+  {DMA1_Channel6_IRQn,      15},
+  {DMA1_Channel7_IRQn,      15},
+  {ADC1_IRQn,               15},
+  {CAN1_TX_IRQn,            15},
+  {CAN1_RX0_IRQn,           15},
+  {CAN1_RX1_IRQn,           15},
+  {CAN1_SE_IRQn,            15},
+  {EXINT9_5_IRQn,           15},
+  {TMR1_BRK_TMR9_IRQn,      15},
+  {TMR1_OVF_TMR10_IRQn,     15},
+  {TMR1_TRG_HALL_TMR11_IRQn,15},
+  {TMR1_CH_IRQn,            15},
+  {TMR2_GLOBAL_IRQn,        15},
+  {TMR3_GLOBAL_IRQn,        15},
+  {TMR4_GLOBAL_IRQn,        15},
+  {I2C1_EVT_IRQn,           15},
+  {I2C1_ERR_IRQn,           15},
+  {I2C2_EVT_IRQn,           15},
+  {I2C2_ERR_IRQn,           15},
+  {SPI1_IRQn,               15},
+  {SPI2_IRQn,               15},
+  {USART1_IRQn,             15},
+  {USART2_IRQn,             15},
+  {USART3_IRQn,             15},
+  {EXINT15_10_IRQn,         15},
+  {ERTCAlarm_IRQn,          15},
+  {OTGFS1_WKUP_IRQn,        15},
+  {SDIO1_IRQn,              15},
+  {TMR5_GLOBAL_IRQn,        15},
+  {UART4_IRQn,              15},
+  {UART5_IRQn,              15},
+  {DMA2_Channel1_IRQn,      15},
+  {DMA2_Channel2_IRQn,      15},
+  {DMA2_Channel3_IRQn,      15},
+  {DMA2_Channel4_5_IRQn,    15},
+  {OTGFS1_IRQn,             15},
+  {CMP1_IRQn,               15},
+  {CMP2_IRQn,               15},
+  {DMA2_Channel6_7_IRQn,    15},
+};
+/* ****************************************************************************************
+ * Inline Function
+ */
+ 
+/* ****************************************************************************************
+ * Private Function
+ */ 
+ 
+/* ****************************************************************************************
+ * Public Function
+ */
+
+/**
+ *
+ */
+void core_at32f415_interrupt_priority(void){
+	int i;
+	int count = (sizeof(nvic_priority_level) / sizeof(nvic_priority_level_t));
+	
+	for(i=0; i<count; i++){
+		NVIC_SetPriority(nvic_priority_level[i].irq, nvic_priority_level[i].priority);
+	}
+}
+
+/* *****************************************************************************************
+ *    End of file
+ */
